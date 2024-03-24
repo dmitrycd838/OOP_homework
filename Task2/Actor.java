938fae455b6396e7a5b3684420da5bbc8e4b3125
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2024-03-24 15:04:33 +0300

    created a task folder 2

---
+++ Task2/Actor.java
@@ -0,0 +1,11 @@
+package Task2;
+
+public abstract class Actor implements ActorBehavior {
+
+    protected String name;
+    protected boolean isMakeOrder;
+    protected boolean isTakeOrder;
+
+    public abstract String getName();
+    
+}
---
+++ Task2/ActorBehavior.java
@@ -0,0 +1,10 @@
+package Task2;
+
+public interface ActorBehavior {
+
+    void setMakeOrder();
+    void setTakeOrder();
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+
+}
