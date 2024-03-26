f9901058020068f1399caaf59e15a882651423d6
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2024-03-26 13:29:33 +0300

    Task3

---
+++ Task3/Student.java
@@ -0,0 +1,20 @@
+package Task3;
+
+public class Student {
+
+    String firstName;
+    String lastName;
+    int age;
+
+    public Student(String firstName, String lastName, int age) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d", firstName, lastName, age);
+    }
+}
