1c10578d5e8486b66de5eae66a74f541dda4daa2
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2024-03-26 13:51:22 +0300

    StudyGroup

---
+++ Task3/StudyGroup.java
@@ -0,0 +1,43 @@
+package Task3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StudyGroup implements Iterable<Student> {
+
+    private List<Student> listOfStudents = new ArrayList<Student>();
+
+    public StudyGroup(List<Student> studList) {
+        this.listOfStudents = studList;
+    }
+
+    public void setListOfStudents(List<Student> studList) {
+        this.listOfStudents = studList;
+    }
+
+    public List<Student> getListOfStudents() {
+        return listOfStudents;
+    }
+
+    @Override
+    public Iterator<Student> iterator() {
+        
+        Iterator<Student> iterStud = new Iterator<Student>() {
+
+            private int index;
+
+            @Override
+            public boolean hasNext() {
+                return index < listOfStudents.size();
+            }
+
+            @Override
+            public Student next() {
+                return listOfStudents.get(index++);
+            }
+        };
+        return iterStud;
+
+    }
+}
