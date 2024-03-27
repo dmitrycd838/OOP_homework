80cde97c55ed33de9383ee9f4ebf038864cd7f9c
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2024-03-27 17:24:49 +0300

    Library

--- Task4/Library.java
+++ Task4/Library.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    
     protected List<LibraryCard<Integer, String>> issuedBooks; 
 
     public Library() {
         this.issuedBooks = new ArrayList<>();
     }
 
-    public <T> void addBook(Book<T> book, int cardNumber, String author, String title) {
-        LibraryCard<Integer, String> card = new LibraryCard<>(cardNumber, "Автор: " + author + ", Название книги: " + title);
+    
+    public <T> void addBook(Book<T> nameBook, int cardNumber, String author, int year) {
+        LibraryCard<Integer, String> card = new LibraryCard<>(cardNumber, "Автор: " + author + ", Год издания: " + year);
         issuedBooks.add(card);
     }
 
-
+    
     public LibraryCard<Integer, String> lendBook(int cardNumber) {
         for (LibraryCard<Integer, String> card : issuedBooks) {
             if (card.getCardNumber() == cardNumber) {
@@ -26,10 +26,11 @@ public class Library {
         return null;
     }
 
-
+    
     public void displayIssuedBooks() {
         for (LibraryCard<Integer, String> card : issuedBooks) {
             System.out.println("Номер карты: " + card.getCardNumber() +  "; Книга: " + card.getBookInfo());
         }
     }
+       
 }
