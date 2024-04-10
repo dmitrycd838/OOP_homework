6150d383ce279b75266c7ab87f54c489a5f1dd2b
Dmitry Churilov <churilov.dmitrycd@mail.ru>
2024-04-10 15:06:46 +0300

    Calculation records

--- Task7/Logging/Log.java
+++ Task7/Logging/Log.java
@@ -4,5 +4,5 @@ public interface Log {
     
     public void writeFile(String expression, String nameFile);
     public String getExpression(Double x, Double y, Double result, Integer operation);
-    
+
 }
