a47ae1290a8e5f8f5890eda9eb26db864d1c7bd5
Dmitry Churilov <churilov.dmitrycd@mail.ru>
2024-04-10 14:06:56 +0300

    Model

---
+++ Task7/Operations/Model.java
@@ -0,0 +1,25 @@
+package Task7.Operations;
+
+public class Model implements Model1{
+    
+    Double x;
+    Double y;
+    
+ 
+    @Override
+    public Double result() {
+        return 0.0;
+    }
+
+    
+    @Override
+    public void setX(Double value) {
+        
+    }
+
+   
+    @Override
+    public void setY(Double value) {
+        
+    }
+}
