8823ccdc4a5c95409e45d0774311e87d6091619b
Dmitry Churilov <churilov.dmitrycd@mail.ru>
2024-04-10 13:56:15 +0300

    Model1

---
+++ Task7/Operations/Model1.java
@@ -0,0 +1,13 @@
+package Task7.Operations;
+
+
+public interface Model1 {
+
+    public Double result();
+
+    public void setX(Double value);
+
+    public void setY(Double value);
+
+    
+}
--- Task7/functions/ComplexOperation.java
+++
@@ -1,9 +0,0 @@
-package Task7.functions;
-
-import Task7.domen.ComplexNumber;
-
-public interface ComplexOperation {
-
-     ComplexNumber operate(ComplexNumber num1, ComplexNumber num2);
-
-}
