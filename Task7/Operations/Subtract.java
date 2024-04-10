22ce82668a38997c37c2e07ca5083d1e3714fa78
Dmitry Churilov <churilov.dmitrycd@mail.ru>
2024-04-10 14:13:20 +0300

    Subtract

---
+++ Task7/Operations/Subtract.java
@@ -0,0 +1,18 @@
+package Task7.Operations;
+
+public class Subtract extends Model{
+    @Override
+    public Double result() {
+        return x - y;
+    }
+    
+    @Override
+    public void setX(Double value) {
+        super.x = value;
+    }
+
+    @Override
+    public void setY(Double value) {
+        super.y = value;
+    }
+}
